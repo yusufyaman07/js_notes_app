1d257825bf3c2d4e5f2d1cb6b276e6ba4bf3880a
Yusuf Yaman <148998418+yusufyaman07@users.noreply.github.com>
2024-04-15 18:04:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// TODO-1: Elementlerin seçilmesi ve başlangıç değişkenlerinin tanımlanması
+
 const addBox = document.querySelector(".add-box"),
   popupBox = document.querySelector(".popup-box"),
   popupTitle = popupBox.querySelector("header p"),
@@ -21,11 +21,11 @@ const months = [
   "November",
   "December",
 ];
-const notes = []; // Notlar geçici olarak bu dizide tutulacak
+const notes = []; 
 let isUpdate = false,
   updateId;
 
-// TODO-2: Yeni not ekleme butonuna tıklandığında popup açılması
+
 addBox.addEventListener("click", () => {
   popupTitle.innerText = "Add a new Note";
   addBtn.innerText = "Add Note";
@@ -33,7 +33,7 @@ addBox.addEventListener("click", () => {
   document.querySelector("body").style.overflow = "hidden";
 });
 
-// TODO-3: Popup kapatma butonunun işlevselliğinin eklenmesi
+
 closeIcon.addEventListener("click", () => {
   isUpdate = false;
   titleTag.value = descTag.value = "";
@@ -41,7 +41,7 @@ closeIcon.addEventListener("click", () => {
   document.querySelector("body").style.overflow = "auto";
 });
 
-// TODO-4: Notları göstermek için fonksiyon
+
 function showNotes() {
   document.querySelectorAll(".note").forEach((li) => li.remove());
   notes.forEach((note, id) => {
@@ -67,7 +67,7 @@ function showNotes() {
 }
 showNotes();
 
-// TODO-5: Ayarlar menüsünü göstermek/gizlemek için fonksiyon
+
 function showMenu(elem) {
   elem.parentElement.classList.add("show");
   document.addEventListener("click", (e) => {
@@ -77,7 +77,7 @@ function showMenu(elem) {
   });
 }
 
-// TODO-6: Seçilen notu silmek için fonksiyon
+
 function deleteNote(noteId) {
   let confirmDel = confirm("Are you sure you want to delete this note?");
   if (!confirmDel) return;
@@ -85,7 +85,7 @@ function deleteNote(noteId) {
   showNotes();
 }
 
-// TODO-7: Notu güncellemek için kullanıcı arayüzünü hazırlayan fonksiyon
+
 function updateNote(noteId, title, filterDesc) {
   let description = filterDesc.replaceAll("<br/>", "\r\n");
   updateId = noteId;
@@ -97,7 +97,7 @@ function updateNote(noteId, title, filterDesc) {
   addBtn.innerText = "Update Note";
 }
 
-// TODO-8: Yeni not eklemek veya mevcut notu güncellemek için buton işlevselliği
+
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let title = titleTag.value.trim(),
